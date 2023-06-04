cdb5a0ceef966797699428aaa5ea419481cc8a87
Nylio-prog <nils.cercariolo@gmail.com>
2023-06-04 02:21:16 -0700

    captions added to data + button automatically checks if vid loaded

--- background.js
+++ background.js
@@ -1,19 +1,44 @@
-chrome.runtime.onMessage.addListener(function (message) { //Listens to the pop up message
-    if (message.activateExtension) {
-        chrome.tabs.onUpdated.addListener(newYoutubeVid);
-    } else if (message.deactivateExtension){
-        chrome.tabs.onUpdated.removeListener(newYoutubeVid);
-    }
+let extensionActive = false;
+
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.activateExtension) {
+    extensionActive = true;
+    getActiveTabInfo().then(({ tabId, tab }) => {
+      newYoutubeVid(tabId, tab);
+    });
+  } else if (message.deactivateExtension) {
+    extensionActive = false;
+  }
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (extensionActive && changeInfo.status === "complete") {
+    newYoutubeVid(tabId, tab);
+  }
+});
+
+function getActiveTabInfo() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs.length > 0) {
+        const activeTab = tabs[0];
+        const tabId = activeTab.id;
+        const tabUrl = activeTab.url;
+
+        resolve({ tabId, tab: activeTab });
+      }
+    });
   });
+}
 
 function newYoutubeVid(tabId, tab) {
-    if (tab.url && tab.url.includes("youtube.com/watch")){
-        const queryParameters = tab.url.split("?")[1];
-        const urlParameters = new URLSearchParams(queryParameters);
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
 
-        chrome.tabs.sendMessage(tabId, {
-            type: "NEW",
-            videoId: urlParameters.get("v"),
-        });
-    }
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+  }
 }
--- contentScript.js
+++ contentScript.js
@@ -12,64 +12,130 @@
         });
     };
 
+    function extractSimpleText(obj) {
+        const results = [];
+        for (const key in obj) {
+          if (typeof obj[key] === 'object') {
+            results.push(...extractSimpleText(obj[key]));
+          } else if (key === 'simpleText') {
+            results.push(obj[key]);
+          }
+        }
+      
+        return results;
+      }
+
+    const getCaptions = async (currentVideo) => {
+        const param_decoded = '\n\x0b' + currentVideo;
+        const param_encoded = btoa(param_decoded);
+        const url = 'https://www.youtube.com/youtubei/v1/get_transcript?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8';
+        const data = {
+        context: {
+            client: {
+            clientName: 'WEB',
+            clientVersion: '2.9999099'
+            }
+        },
+        params: param_encoded
+        };
+
+        const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+        })
+
+        const json_response = await response.json();
+
+        const captions = extractSimpleText(json_response);  
+
+        return captions;
+    } 
+
       // Function to retrieve video details using the YouTube API
     const getVideoData = async () => {
         try {
             const request = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=" + currentVideo + "&key=" + APIKey;
             const response = await fetch(request);
             const data = await response.json();
+            const captions = await getCaptions(currentVideo);
 
             const videoItem = data.items[0];
             const videoData = {
+                id: currentVideo,
+                url: "https://www.youtube.com/watch?v=" + currentVideo,
                 title: videoItem.snippet.title,
                 channel: videoItem.snippet.channelTitle,
-                captions: ""
+                captions: captions,
+                recentDateWatched: new Date().toISOString()
             };
 
             return videoData;
         
         } catch (error) {
-            throw new Error('Error retrieving video details: ' + error);
+            throw new Error('Error retrieving video details with id:' + currentVideo + " : " + error);
         }
     }
 
     const storeVideoEventHandler = async () => {
-        vidData = await getVideoData();
-        id = currentVideo;
-        url = "https://www.youtube.com/watch?v=" + currentVideo;
-        recentDateWatched = new Date().toISOString()
-        const newVideo = {
-            id: id,
-            url: url,
-            title: vidData.title,
-            channel: vidData.channel,
-            captions: vidData.captions,
-            recentDateWatched: recentDateWatched
-        };
 
+        const statusBtn = document.getElementsByClassName("status-btn")[0];
         videos = await fetchVideos();
     
-        const existingVideo = videos.find(video => video.id === newVideo.id);
-        if (!existingVideo) {
+        const existingVideoIndex = videos.findIndex(video => video.id === currentVideo);
+        if (existingVideoIndex === -1) {
+
+            vidData = await getVideoData();
+
+            const newVideo = {
+                id: vidData.id,
+                url: vidData.url,
+                title: vidData.title,
+                channel: vidData.channel,
+                captions: vidData.captions,
+                recentDateWatched: vidData.recentDateWatched
+            };
+
             chrome.storage.local.set({
             [storage_index]: JSON.stringify([...videos, newVideo].sort((a, b) => a.title - b.title))
             });
-            console.log("Stored a new video");
+            statusBtn.style.filter = 'invert(58%) sepia(64%) saturate(2319%) hue-rotate(78deg) brightness(114%) contrast(131%)'; //To make it green
+
         } else {
-            console.log("Video already stored");
+            videos.splice(existingVideoIndex, 1);
+            chrome.storage.local.set({
+                [storage_index]: JSON.stringify(videos)
+            });
+            statusBtn.style.filter = 'invert(12%) sepia(78%) saturate(7358%) hue-rotate(2deg) brightness(97%) contrast(116%)'; //To make it red
         }
-      };
+
+        
+    };
+
+    function changeColorStatus(btn, index){
+        if (index === -1) {
+            btn.style.filter = 'invert(12%) sepia(78%) saturate(7358%) hue-rotate(2deg) brightness(97%) contrast(116%)'; //To make it red
+        }
+        else{
+            btn.style.filter = 'invert(58%) sepia(64%) saturate(2319%) hue-rotate(78deg) brightness(114%) contrast(131%)'; //To make it green
+        }
+    }
 
     const newVideoLoaded = async () => {
         const statusBtnExists = document.getElementsByClassName("status-btn")[0];
-        
+
+        videos = await fetchVideos();
+        const existingVideoIndex = videos.findIndex(video => video.id === currentVideo);
+
         if (!statusBtnExists){
             const statusBtn = document.createElement("img");
 
             statusBtn.src =  chrome.runtime.getURL("assets/status.png");
             statusBtn.className = "ytp-button " + "status-btn";
             statusBtn.title = "Click to add to the history";
-            statusBtn.style.filter = 'brightness(0) invert(1)'; //To make the png white
+            changeColorStatus(statusBtn, existingVideoIndex);
 
             youtubeRightControls = document.getElementsByClassName("ytp-right-controls")[0];
             youtubePlayer = document.getElementsByClassName("video-stream")[0];
@@ -77,6 +143,10 @@
             youtubeRightControls.prepend(statusBtn);
             statusBtn.addEventListener("click", storeVideoEventHandler);
         }
+        else{
+            changeColorStatus(statusBtnExists, existingVideoIndex);
+        }
+
     }
 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
