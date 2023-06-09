f1a51fdb7994d32b199b083ffebcc531109fa366
Nylio-prog <nils.cercariolo@gmail.com>
2023-06-09 22:24:09 +0900

    #38 added data around video list but still missing how to put the clips number in the middle right

--- website.js
+++ website.js
@@ -4,7 +4,9 @@ var cur_id;
 var search_word;
 
 function get_captions(id){
+    console.log(id);
     var caps = cap_map.get(id);
+    console.log(caps);
     const cap_div = document.getElementsByClassName("captions-list")[0];
     cap_div.innerHTML = "";
     var num_of_captions = document.getElementById("number-of-captions");
@@ -13,7 +15,7 @@ function get_captions(id){
         var caption = document.createElement('p');
         caption.classList.add("caption");
         var sub_str = x[1].toLowerCase().split(search_word);
-        caption.innerHTML = '<span class="first-word">' + x[0] + '</span> ' + sub_str[0] + search_word + sub_str[1];
+        caption.innerHTML = '<span class="first-word">' + x[0] + '</span> ' + sub_str[0] + '<span class="highlight">' + search_word + '</span>' + sub_str[1];
         cap_div.appendChild(caption);
     }
 }
@@ -26,19 +28,29 @@ function display_vids(search_data){
         var arr_of_vids = JSON.parse(data.history_videos);
         const st = new Set();
         var ids = [];
+        var titles = [];
+        var channels = [];
+        var dates = [];
+        var number_of_captions = [];
         for(var j = 0; j < arr_of_vids.length; j++){
             var it = arr_of_vids[j];
             var list_of_caps = [];
+            var counter_of_caps = 0;
             for(var i = 1; i < it.captions.length; i += 2){
                 var str = it.captions[i];
                 var low_case = str.toLowerCase();
                 if (low_case.includes(search_data)){
                     var pair = [it.captions[i-1], str];
                     list_of_caps.push(pair);
+                    counter_of_caps +=1;
                 }
             }
             if(list_of_caps.length != 0){
                 ids.push(it.id);
+                titles.push(it.title);
+                channels.push(it.channel);
+                dates.push(it.recentDateWatched.split('T')[0]);
+                number_of_captions.push(counter_of_caps);
                 cap_map.set(it.id, list_of_caps);
             }
         }
@@ -54,7 +66,6 @@ function display_vids(search_data){
         const one_vid = document.getElementsByClassName("video-frame")[0];
         one_vid.innerHTML = '';
 
-        //https://www.youtube.com/watch?v=zz_SjeT_-M4&ab_channel=Naritsa to https://www.youtube.com/embed/zz_SjeT_-M4
         if(ids.length > 0){
             var src = "https://www.youtube.com/embed/" + ids[0];
             var iframe = document.createElement('iframe');
@@ -64,14 +75,50 @@ function display_vids(search_data){
             one_vid.appendChild(iframe);
         }
 
-        for(let it of ids){
-            var src = "https://www.youtube.com/embed/" + it;
+        for (var i = 1; i < ids.length; i += 1) {
+            var src = "https://www.youtube.com/embed/" + ids[i];
+            var container = document.createElement('div');
+            container.classList.add('video-container');
+          
+            var videoBox = document.createElement('div');
+            videoBox.classList.add('video-box');
+          
             var iframe = document.createElement('iframe');
             iframe.style.width = '240px';
             iframe.style.height = '160px';
             iframe.src = src;
-            vid_div.appendChild(iframe);
-        }
+            videoBox.appendChild(iframe);
+          
+            container.appendChild(videoBox);
+          
+            var videoInfo = document.createElement('div');
+            videoInfo.classList.add('video-info');
+          
+            var videoTitle = document.createElement('p');
+            videoTitle.classList.add('video-title');
+            videoTitle.innerText = titles[i];
+            videoInfo.appendChild(videoTitle);
+          
+            var channelName = document.createElement('p');
+            channelName.classList.add('channel-name');
+            channelName.innerText = channels[i];
+            videoInfo.appendChild(channelName);
+          
+            var watchedDate = document.createElement('p');
+            watchedDate.classList.add('watched-date');
+            watchedDate.innerText = "Watched on: " + dates[i];
+            videoInfo.appendChild(watchedDate);
+          
+            var captionsNumber = document.createElement('p');
+            captionsNumber.classList.add('video-list-num-captions');
+            captionsNumber.innerText = number_of_captions[i] + " clips";
+            videoInfo.appendChild(captionsNumber);
+          
+            container.appendChild(videoInfo);
+            vid_div.appendChild(container);
+          }
+          
+          
     });
 }
 
